e14b00526f44ece0e9efc6dfd73accd829774966
tensa322 <tensa322@gmail.com>
2022-09-28 18:17:12 +0300

    4th app not ended

--- src/App.js
+++ src/App.js
@@ -6,12 +6,23 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+   const [users, setUsers] = React.useState([])
+   React.useEffect(() => {
+      fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then(json => {
+         setUsers(json.data)
+      }).catch(err=>{
+         console.warn(err)
+         alert('Ошибка при получении пользователей')
+      })
+   }, [])
+   return (
+      <div className="App">
+         <Users />
+         {/* <Success /> */}
+      </div>
+   );
 }
 
 export default App;
